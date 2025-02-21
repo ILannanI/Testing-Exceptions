f8826faed9e910c4dc64dd72993af499f5933bbd
ILannanI <Sl0989031@otc.edu>
2025-02-20 23:09:05 -0600

    Testing Exception

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+public class InvalidUsernameException : Exception
+{
+    public InvalidUsernameException(string message) : base(message) { }
+}
+
+public class InvalidPasswordException : Exception
+{
+    public InvalidPasswordException(string message) : base(message) { }
+}
+
+public class UserValidator
+{
+    public void ValidateCredentials(string username, string password)
+    {
+        if (string.IsNullOrEmpty(username) || username.Length < 5)
+        {
+            throw new InvalidUsernameException("Username must be at least 5 characters.");
+        }
+
+        if (string.IsNullOrEmpty(password) || password.Length < 8)
+        {
+            throw new InvalidPasswordException("Password must be at least 8 characters.");
+        }
+    }
+}
---
+++ UserValidatorTests/MSTestSettings.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UserValidatorTests
+{
+    [TestClass]
+    public class UserValidatorTest
+    {
+        private UserValidator userValidator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            userValidator = new UserValidator();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidUsernameException))]
+        public void Test_InvalidUsername_ShouldThrowInvalidUsernameException()
+        {
+            string invalidUsername = "usr";  
+            string validPassword = "ValidPass1";
+
+            userValidator.ValidateCredentials(invalidUsername, validPassword);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidPasswordException))]
+        public void Test_InvalidPassword_ShouldThrowInvalidPasswordException()
+        {
+            // Arrange
+            string validUsername = "ValidUser";
+            string invalidPassword = "short"; 
+
+            // Act
+            userValidator.ValidateCredentials(validUsername, invalidPassword);
+
+            // Assert is handled by the ExpectedException attribute
+        }
+
+        [TestMethod]
+        public void Test_ValidCredentials_ShouldNotThrowAnyException()
+        {
+            // Arrange
+            string validUsername = "ValidUser";
+            string validPassword = "ValidPassword1";
+
+            try
+            {
+                userValidator.ValidateCredentials(validUsername, validPassword);
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Exception should not be thrown for valid credentials.");
+            }
+        }
+    }
+}
+
